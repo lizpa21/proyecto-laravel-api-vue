b6dd2ce3f58389bcabf86ed73a7c9b3f74a68af2
Lizeth <lizeth.padilla@gmail.com>
2023-08-29 21:14:36 -0400

    sanctum

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,11 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     //
     public function funIngresar(Request $request){
+        // validar
+        $credenciales = $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+        //verificar y autenticar
+        if(!Auth::attempt($credenciales)){
+            return response()->json("No autenticado",401);
+
+        }
+        
+        //generamos token
+
+        $usuario =Auth::user();
+        //$token = $usuario->createToken("token personal")->plainTextToken;
+        $token = $request->user()->createToken("token personal")->plainTextToken;
+        return response()->json([
+            "acces_token"=> $token,
+            "token_type" =>   "Bearer",
+            "usuario" => $request->user()
+
+
+        ]);
 
     }
     public function funRegistro(Request $request){
@@ -27,10 +51,20 @@ class AuthController extends Controller
         // respuesta
         return response()->json(["message" => "Usuario registrado", "data"=> $user], 201);
     }
-    public function funPerfil(){
+    public function funPerfil(Request $request){
+
+        //$usuario = $request->user();
+        //$usuario = $request->user()->persona;
+       // $usuario = Auth::user()->with("persona");
+        $usuario = User::with('persona')->find(Auth::user()->id);
+        return response()->json($usuario,200);
         
     }
-    public function funSalir(){
+    public function funSalir(Request $request){
+        $usuario=$request->user();
+        $usuario->tokens->delete();
+
+        return response()->json(["message"=>"log out"]);
         
     }
 }
